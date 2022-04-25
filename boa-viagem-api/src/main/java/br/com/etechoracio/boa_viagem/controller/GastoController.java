4aac1d41dad388a0e65cf9fee9774b0ad9c5f601
beatriz-camila <beatrizcamila@test.com>
2022-04-25 09:01:54 -0300

    Abrobinha

--- boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
+++ boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
@@ -1,7 +1,15 @@
 package br.com.etechoracio.boa_viagem.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +20,92 @@ import br.com.etechoracio.boa_viagem.repository.GastoRepository;
 @RequestMapping("/gastos")
 public class GastoController {
 	
+	@Autowired
 	private GastoRepository repository;
 	
+	@GetMapping
 	public List<Gasto> listarTodos(){
 		return repository.findAll();
 	}
-}
+	
+/*	@GetMapping("/{id}")
+	public ResponseEntity<Gasto> buscarPorId(@PathVariable Long id) {
+		Optional<Gasto> existe = repository.findById(id);
+		if (existe.isPresent()) {
+			return ResponseEntity.ok(existe.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+		
+	}*/
+	@GetMapping("/{id}")
+	public ResponseEntity<Gasto> buscarPorId(@PathVariable Long id) {
+		Optional<Gasto> existe = repository.findById(id);
+		return existe.isPresent() ? ResponseEntity.ok(existe.get())
+								  : ResponseEntity.notFound().build();
+		}
+		
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> excluir(@PathVariable long id) {
+		boolean existe = repository.existsById(id);
+		if (existe) {
+			repository.deleteById(id);
+			return ResponseEntity.ok().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<Gasto> inserir(@RequestBody Gasto obj) {
+		repository.save(obj);
+		return ResponseEntity.ok(obj);
+	}
+		
+	}
+	
+/*
+Contemplem o mago
+Com seus poderes
+Incríveis poderes
+Sob o olhar do necromante
+A escada prateada vai se erguer
+As pessoas maravilhadas
+Com seus olhos cheios de poder
+Comida fria vai esquentar ao enfeitiçar
+Balançando as suas mãos
+O cachorro quente explodirá
+Na presença do grande mago
+O trânsito para de repente
+Pode atravessar a rua
+Com os carros parados na sua frente
+EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+Na tv mudam-se os canais
+Sem que saia do sofáaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+Sua varinha pega então
+Pra reclinar-se no ar
+Pela luz fraca do sol negro do reino dos sonhos.
+O mago sobe as cataratas congeladas de Voldrini
+Em busca de Celestia, a guardiã do poder infinito
+Quando de repente um terrível Garlon aparece
+E ataca com gelo, mas o mago é implacável
+O Garlon ruge e libera um vento muito sinistro,
+Mas o mago é implacável.
+O Garlon invoca as pedras de Prophynia,
+Mas o mago é implacável.
+Invocando os poderes dos ancestrais,
+O mago conjura o fogo sagrado
+E lança seu feitiço na larva derretida de um Gort insaciável.
+(Obrigado, Gort!)
+Ahh, Celestia. Acho que você vai adorar isso
+O mago fica diante do precipício do poder definitivo,
+Os portões se abrem para revelar...
+Hum... Quanto tempo eu fiquei dormindo?
+Faminto por causa da última missão
+O mago quer lanchar
+Traça o rumo do prazer
+Pro habitual lugar
+Ele é o magoooooooooooooooooooooooooooooooooooooooo
+O místico magoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+*/
