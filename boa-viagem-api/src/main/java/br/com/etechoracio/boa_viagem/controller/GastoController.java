f30ccf42e7b20c9eb8fb67232807a49ff26ea102
joao-camila <joaodoidao153@gmail.com>
2022-05-02 08:50:13 -0300

    Editando o Controller

--- boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
+++ boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,7 +63,18 @@ public class GastoController {
 		repository.save(obj);
 		return ResponseEntity.ok(obj);
 	}
-		
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Gasto> atualizar(@PathVariable Long id, 
+										   @RequestBody Gasto gasto) {
+		boolean existe = repository.existsById(id);
+		if (!existe) {
+			return ResponseEntity.notFound().build();
+		}
+		repository.save(gasto);
+		return ResponseEntity.ok(gasto);
+	}
+
 	}
 	
 /*
--- boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/ViagemController.java
+++ boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/ViagemController.java
@@ -1,9 +1,19 @@
 package br.com.etechoracio.boa_viagem.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import br.com.etechoracio.boa_viagem.entity.Viagem;
 import br.com.etechoracio.boa_viagem.repository.ViagemRepository;
@@ -11,10 +21,47 @@ import br.com.etechoracio.boa_viagem.repository.ViagemRepository;
 @RestController
 @RequestMapping("/viagem")
 public class ViagemController {
-
+	
+	@Autowired
 	private ViagemRepository repository;
 	
+	@GetMapping
 	public List<Viagem> listartodos(){
 		return repository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Viagem> buscarPorId(@PathVariable Long id) {
+		Optional<Viagem> existe = repository.findById(id);
+		return existe.isPresent() ? ResponseEntity.ok(existe.get())
+								  : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> excluir (@PathVariable Long id) {
+		boolean existe = repository.existsById(id);
+		if (existe) {
+			repository.deleteById(id);
+			return ResponseEntity.ok().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<Viagem> inserir(@RequestBody Viagem obj) {
+		repository.save(obj);
+		return ResponseEntity.ok(obj);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Viagem> atualizar(@PathVariable Long id, 
+											@RequestBody Viagem viagem) {
+			boolean existe = repository.existsById(id);
+			if (!existe) {
+				return ResponseEntity.notFound().build();
+			}
+			repository.save(viagem);
+			return ResponseEntity.ok(viagem);
 	}
 }
